78ac6a28c0e4c3a543584e1e5632df0821a83cbf
Jonathan Lister <pjl@hilltopbarn.info>
2020-02-08 08:55:50 +0000

    Fix update-files

--- update-files.sh
+++ update-files.sh
@@ -4,15 +4,17 @@
 
 cd ~/bin
 
-./getfile.sh https://github.com/freddieFishCake/webcam-scripts/blob master get_file.sh
-./getfile.sh https://github.com/freddieFishCake/webcam-scripts/blob master get-weather.sh
-./getfile.sh https://github.com/freddieFishCake/webcam-scripts/blob master interpret.sh
-./getfile.sh https://github.com/freddieFishCake/webcam-scripts/blob master resize.sh
-./getfile.sh https://github.com/freddieFishCake/webcam-scripts/blob master take-a-pic.sh
-./getfile.sh https://github.com/freddieFishCake/webcam-scripts/blob master truncate-logs.sh
-./getfile.sh https://github.com/freddieFishCake/webcam-scripts/blob master update-files.sh
-./getfile.sh https://github.com/freddieFishCake/webcam-scripts/blob master upload.sh
-./getfile.sh https://github.com/freddieFishCake/webcam-scripts/blob master webcam.sh
+URL=https://raw.githubusercontent.com/freddieFishCake/webcam-scripts
+
+./get-file.sh $URL master get-file.sh
+./get-file.sh $URL master get-weather.sh
+./get-file.sh $URL master interpret.sh
+./get-file.sh $URL master resize.sh
+./get-file.sh $URL master take-a-pic.sh
+./get-file.sh $URL master truncate-logs.sh
+./get-file.sh $URL master update-files.sh
+./get-file.sh $URL master upload.sh
+./get-file.sh $URL master webcam.sh
 
 chmod +x *.sh
 
